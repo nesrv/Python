b4453284b9220b1aa537af8d24a752ac5341ab9b
Boss <ne_srv@mail.ru>
2021-03-28 20:31:55 +0300

    28 марта

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-print ("hello git")
+# Автор: А.Н. Носкин
 
-for i in range(10):
-    print (i)
+""" идея А.М. Кабанов"""
+s =  open("k7a-2.txt"). readline() # считали строку
+
+k = 0
+c1 = 'BCD'# строка проверки первого символа
+c2 = 'BDE'# строка проверки второго символа
+c3 = 'BCE'# строка проверки третьего символа
+for i in range(len(s)-2):
+    if s[i] in c1 and s[i+1] in c2 and s[i+2] in c3 \
+       and s[i]!=s[i+1] and s[i+1]!=s[i+2]: # проверка повтора символов
+      k += 1
+print(k)
